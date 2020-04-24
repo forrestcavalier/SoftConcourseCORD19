eb9aebfad93ad3c75de5beef18b4b7ae0ed33b16
forrestcavalier <forrest-github@mibsoftware.com>
2020-04-24 14:25:56 -0400

    Suppport expressions in filter text.

--- parser/selected.js
+++ parser/selected.js
@@ -4,6 +4,116 @@ import dateFormatter from "./dateFormatter.js"
 
 let {selector,counts,idxWords,obj,header,setFirstTime,getFirstTime,cache_Qualifiers} = getParserState()
 
+function searchParse(s)
+{
+	//DEBUG: should adjacent words not enclosed in double quotes get automatically put in double quotes?
+	var parts = s.replace(/\(/g,' ( ').replace(/\)/g,' ) ').split(' ');
+	var i = 0;
+	while(i < parts.length) {
+		if (parts[i]=='') {
+			parts.splice(i,1);
+		} else if (parts[i].charCodeAt(0) == 0x22) {
+			var i2 = i+1;
+			while(i2 < parts.length) {
+				parts[i] += ' ' + parts[i2];
+				if (parts[i2].substr(parts[i2].length-1)=='\"') {
+					parts.splice(i2,1);
+					break;
+				}
+				parts.splice(i2,1);
+			}
+//			WScript.Echo('I-19 ' + i2 + ' of ' + parts.length);
+			if (i2 >= parts.length) {
+				parts[i] += '\x22'; /* Force close */
+			}
+			parts[i] = parts[i].replace(/ \( /g,'(').replace(/ \) /g,')');
+			i++;
+		} else {
+			//ELSE: HANDLE punctuation like splitWords does.
+			if (parts[i]=='OR' || parts[i]=='AND' || parts[i]=='NOT') {
+			} else {
+				parts[i] = parts[i].toLowerCase();
+			}
+			i++;
+		}
+	}
+	return parts;
+}
+
+function hasWord(word, list)
+{
+	return list.indexOf(word) >= 0;
+}
+function searchReduce(parts,iStart,iStop,evalTerm,args)
+{
+	var i = iStart;
+	do {
+		if (0) {//For debugging
+			var show = '';
+			show = parts.slice(0,iStart).join('|');
+			show += '[';
+			show += parts.slice(iStart,iStop).join('|');
+			show += ']';
+			show += parts.slice(iStop).join('|');
+			WScript.Echo("I-67 " + show);
+		}
+		if (parts[i]=='(') {
+			/* Search forward */
+			var i2 = i+1;
+			var nest = 1;
+			while(nest > 0 && i2 < iStop) {
+				if (parts[i2] == '(') {
+					nest++;
+				} else if (parts[i2] == ')') {
+					nest--;
+				}
+				i2++;
+			}
+			parts.splice(i2-1,1);
+			parts.splice(i,1);
+			searchReduce(parts,i,i2-2,evalTerm,args);
+			iStop -= i2-i-1;
+		} else if (parts[i]=='NOT') {
+			searchReduce(parts,i+1,iStop,evalTerm,args);
+			//WScript.Echo('I-87 ' + parts.join('|'));
+			if (parts[i+1]===false) {
+				parts[i+1] = true;
+			} else {
+				parts[i+1] = false;
+			}
+			parts.splice(i,1);
+			iStop = i+1;
+			
+		} else if (parts[i]===true || (true==(parts[i] = evalTerm(parts[i],args)))) {
+			if (i + 1 < iStop) {
+				if (parts[i+1] == 'OR') {
+					parts.splice(i+1,iStop-i-1); /* Short circuit */
+					iStop = i+1;
+				} else if (parts[i+1] == 'AND') {
+					parts.splice(i,2);
+					iStop -= 2;
+				} else {
+					parts.splice(i,1);
+					iStop -= 1;
+				}
+			}
+		} else {
+			if (i + 1 < iStop) {
+				if (parts[i+1] != 'OR') {
+					parts.splice(i+1,iStop-i-1); /* Short circuit */
+					iStop = i+1;
+				} else {
+					parts.splice(i,2);
+					iStop -= 2;
+				}
+			}
+		}
+	} while(iStop > iStart+1);
+	if (parts[iStart]!==true && parts[iStart]!==false) {
+		parts[iStart] = evalTerm(parts[iStart],args);
+	}
+}
+
 export default function selected(selector, obj) {
 	if (selector.hasOwnProperty('runPT')) {
 		selector.cpmid++
@@ -28,9 +138,9 @@ export default function selected(selector, obj) {
 		selector.wantDP = {};
 		var hasFalse = false;
 
-		var DPstart = selector.boxes.dropdownDPstart
+		var DPstart = selector.boxes.dropdownDPstart;
 		//DPstart = DPstart ? DPstart.substr(0,DPstart.indexOf(' (')) : '';
-		var DPend = selector.boxes.dropdownDPend
+		var DPend = selector.boxes.dropdownDPend;
 		//DPend = DPend ? DPend.substr(0,DPend.indexOf(' (')) : '';
 
 		let DPstartTime = new Date(DPstart).getTime()
@@ -71,7 +181,7 @@ export default function selected(selector, obj) {
 		}
 		if (selector.boxes.searchTerms) {
 			//console.log("I-253");
-			selector.terms = splitWords(selector.boxes.searchTerms, true);
+            selector.terms = searchParse(selector.boxes.searchTerms, true);
 			/* DEBUG: handle OR, AND, double quotes, parens, etc. */
 
 		} else {
@@ -117,26 +227,12 @@ export default function selected(selector, obj) {
 		}
 	}
 	if (selector.terms) {
-		var terms = selector.terms;
+		var terms = selector.terms.slice(0);
 		var words = splitWords((obj.AB ? obj.AB.join(' ') : '') + ' ' + obj.TI.join(' '),false);
-		for(var iWord = 1;iWord < terms.length;iWord += 2) {
-			if (terms[iWord] == 'AND') {
-				continue;
-			} else if (words.indexOf(terms[iWord]) >= 0) {
-				if (iWord + 2 < terms.length && terms[iWord+2] == 'OR') {
-					iWord = terms.length;
-					break;
-				}
-			} else {
-				if (iWord + 2 < terms.length && terms[iWord+2] == 'OR') {
-					/* Keep looking */
-					iWord += 2;
-				} else {
-					break;
-				}
-			}
-		}
-		if (iWord < terms.length) {//Word not found.
+        searchReduce(terms,0,terms.length,hasWord,words);
+        if (terms[0]===true) {
+            return true;
+		} else {
 			return false;
 		}
 	}
